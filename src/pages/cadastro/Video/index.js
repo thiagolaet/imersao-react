49dee97d3f55f9bfef039e4832c29b4e24244405
Thiago Laet <thi4golaet@gmail.com>
2020-07-28 11:54:52 -0300

    Slider no Carousel

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function CadastroVideo() {
     return (
       <Template>
-        <h1>Cadastro de vídeo</h1>
+        <h1>Cadastro de Vídeo</h1>
 
         <Link to="/cadastro/categoria">
           Cadastrar Categoria
