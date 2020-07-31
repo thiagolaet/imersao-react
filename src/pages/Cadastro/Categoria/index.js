62ede500ae5fd5363ff42cef1b541eef47bfd38c
Thiago Laet <thi4golaet@gmail.com>
2020-07-31 20:20:34 -0300

    Cadastro de vídeo

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -5,28 +5,19 @@ import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 import { Form } from './styles';
 import './index.css';
+import useForm from '../../../hooks/useForm'
 
 function CadastroCategoria() {
   // useState() atribui um valor inicial ao state
   const valoresIniciais = {
-    nome: '',
+    titulo: '',
     descricao: '',
     cor: '#000',
   };
 
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(chave, valor) {
-    setValues({
-      ...values,
-      [chave]: valor,
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    setValue(infosDoEvento.target.name, infosDoEvento.target.value);
-  }
 
   // Primeiro colocamos o efeito que queremos que ocorra e dentro do array específicamos quando queremos q ele ocorra, se deixarmos o array vazio ele irá ocorrer apenas 1 vez quando a página for carregada
 
@@ -49,20 +40,21 @@ function CadastroCategoria() {
     <Template>
       <h1>
         Cadastro de Categoria:
-        {values.nome}
+        {values.titulo}
       </h1>
 
       <Form onSubmit={function handleSubmit(event) {
         event.preventDefault();
 
         setCategorias([
-          ...categorias, values,
+          ...categorias, 
+          values,
         ]);
 
-        setValue(valoresIniciais);
+        clearForm();
       }}
       >
-        <FormField label="Nome da Categoria" type="text" name="nome" value={values.nome} onChange={handleChange} />
+        <FormField label="Título da Categoria" type="text" name="titulo" value={values.titulo} onChange={handleChange} />
 
         <FormField label="Descrição" type="textarea" name="descricao" value={values.descricao} onChange={handleChange} />
 
@@ -84,7 +76,7 @@ function CadastroCategoria() {
       <ul>
         {categorias.map((categoria) => (
           <li key={categoria.id}>
-            {categoria.nome}
+            {categoria.titulo}
           </li>
         ))}
       </ul>
--- src/pages/Cadastro/Video/index.js
+++ src/pages/Cadastro/Video/index.js
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Template from '../../../components/Template';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import FormField from '../../../components/FormField';
+import useForm from '../../../hooks/useForm';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
-    return (
-      <Template>
-        <h1>Cadastro de Vídeo</h1>
+  const history = useHistory();
+  const [ categorias, setCategorias ] = useState([]);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
+  const { handleChange, values } = useForm({
+    titulo: '',
+    url: '',
+    categoria: '',
+  });
+
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+  }, []);
+
+  return (
+    <Template>
+      <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={e => {
+        e.preventDefault();
+        history.push('/');
+
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+
+        console.log(categoriaEscolhida);
+
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: categoriaEscolhida.id,
+        })
+          .then(() => {
+            console.log('Cadastrou um sucesso!');
+            history.push('/');
+          })
+      }}>
+
+        <FormField 
+        label="Título do Vídeo" 
+        type="text" 
+        name="titulo" 
+        value={values.titulo}
+        onChange={handleChange} />
+
+        <FormField 
+        label="URL do Vídeo" 
+        type="text" 
+        name="url" 
+        value={values.url}
+        onChange={handleChange} />
+
+        <FormField
+        label="Categoria"
+        name="categoria"
+        value={values.categoria}
+        onChange={handleChange}
+        suggestions={categoryTitles}
+        />
+
+        <Button type="submit" className="botaoCadastrar">Cadastrar</Button>
 
         <Link to="/cadastro/categoria">
-          Cadastrar Categoria
+        Cadastrar Categoria
         </Link>
 
-      </Template>
-    );
+      </form>
+
+    </Template>
+  );
 }
 
 export default CadastroVideo;
