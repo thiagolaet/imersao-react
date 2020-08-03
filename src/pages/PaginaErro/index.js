0c4a471ebc9ad0da2a639f330122806e81ff0c11
Thiago Laet <thi4golaet@gmail.com>
2020-08-03 11:58:03 -0300

    Página 404 personalizada

--- src/pages/PaginaErro/index.js
+++ src/pages/PaginaErro/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Template from '../../components/Template';
-import { Title } from './styles'
+import { ImageField, Image, Link } from './styles';
+import Erro from '../../assets/Erro.png'
+
+
 
 function PaginaErro() {
     return (
       <Template>
-        <Title>
-          404
-        </Title>
+        <ImageField>
+          <Image alt="Erro 404" src={Erro}/>
+          <Link href="https://stories.freepik.com/web">Illustration by Freepik Stories</Link>
+        </ImageField>
       </Template>
     );
 }
--- src/pages/PaginaErro/styles.js
+++ src/pages/PaginaErro/styles.js
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
 
-export const Title = styled.h3`
-    font-style: normal;
-    font-weight: normal;
-    font-size: 98px;
-    width: 100%;
-    text-align: center;
-    line-height: 1;
-    display: inline-block;
-    padding: 0 20px;
-    border-radius: 4px;
+export const ImageField = styled.div`
+    display: flex;  
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const Image = styled.img` 
+    object-fit: scale-down;
+    height: 60vh;
+    align-self: center;
+`;
+
+export const Link = styled.a` 
+    color: var(--primary);
+    font-size: 14px;
+    text-decoration: none;
+    align-self: center;
+    margin-bottom: 20px;
 `;
